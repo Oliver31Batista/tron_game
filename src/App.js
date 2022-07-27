af60450a946b9dbdbc044d42ce395c1e8921683a
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-07-26 22:15:02 -0400

    restriction added to the players, when they change direction, they cant move back

--- src/App.js
+++ src/App.js
@@ -1,60 +1,56 @@
-import React, { useEffect, useReducer } from 'react';
-import './App.css';
-import Board from './components/Board';
-import { PLAYER_ONE, PLAYER_TWO } from './config/const';
-import useInterval from './hooks/useInterval';
-import sumCoordinates from './utils/sumCoordinates';
+import React, { useEffect, useReducer } from "react";
+import Board from "./components/Board";
+import { PLAYER_ONE, PLAYER_TWO } from "./config/const";
+import useInterval from "./hooks/useInterval";
+import sumCoordinates from "./utils/sumCoordinates";
+import playerCanChangeToDirection from "./utils/playerCanChangeToDirection";
+import "./App.css";
 
-const initialState = [
-  PLAYER_ONE, PLAYER_TWO
-]
+const initialState = [PLAYER_ONE, PLAYER_TWO];
 
-function updateGame(players, action){
-  if(action.type === 'move'){
-    const newPlayers = players.map(player => ({
+function updateGame(players, action) {
+  if (action.type === "move") {
+    const newPlayers = players.map((player) => ({
       ...player,
-      position: sumCoordinates(player.position, player.direction)
+      position: sumCoordinates(player.position, player.direction),
     }));
     return newPlayers;
   }
-  if (action.type === 'changeDirection'){
-    console.log(players[0].keys)
-    const newPlayers = players.map(player => (
-      {
-        ...player,
-        direction: player.keys[action.key] ? player.keys[action.key] : player.direction
-        
-      }
-  ));
+  if (action.type === "changeDirection") {
+    console.log(players[0].keys);
+    const newPlayers = players.map((player) => ({
+      ...player,
+      direction:
+        player.keys[action.key] &&
+        playerCanChangeToDirection(player.direction, player.keys[action.key])
+          ? player.keys[action.key]
+          : player.direction,
+    }));
     return newPlayers;
   }
 }
 
 function App() {
+  const [players, gameDispatch] = useReducer(updateGame, initialState);
 
-  const [players, gameDispatch] = useReducer(updateGame, initialState)
+  useInterval(function () {
+    gameDispatch({ type: "move" });
+  }, 1000);
 
-  useInterval(function(){
-    gameDispatch({type: 'move'});
-
-  }, 1000)
-
-  useEffect(function(){
-    function handleKeyPress(event){
+  useEffect(function () {
+    function handleKeyPress(event) {
       const key = `${event.keyCode}`;
-      gameDispatch({ type: 'changeDirection', key});
+      gameDispatch({ type: "changeDirection", key });
     }
 
-    document.addEventListener('keydown', handleKeyPress);
+    document.addEventListener("keydown", handleKeyPress);
 
-    return function cleanUp(){
-      document.removeEventListener('keydown', handleKeyPress);
-    }
+    return function cleanUp() {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
   }, []);
 
-  return (
-    <Board players={players} />
-  );
+  return <Board players={players} />;
 }
 
 export default App;
